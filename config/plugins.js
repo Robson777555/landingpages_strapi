c7bef79a6ebe67d8478c86cc7796a65082a9fe6e
Robson777555 <136523255+Robson777555@users.noreply.github.com>
2025-03-05 23:40:41 -0300

    Atualização das configurações do Imgur

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,7 @@ module.exports = {
       provider: path.resolve(__dirname, '../extensions/upload/providers/imgur'), // Caminho absoluto para o provedor personalizado
       providerOptions: {
         clientId: process.env.IMGUR_CLIENT_ID,
+        clientSecret: process.env.IMGUR_CLIENT_SECRET,
       },
     },
   },
--- extensions/upload/providers/imgur.js
+++ extensions/upload/providers/imgur.js
@@ -9,19 +9,31 @@ module.exports = {
         formData.append('image', file.buffer, { filename: file.name });
 
         try {
-          const response = await axios.post('https://api.imgur.com/3/image', formData, {
+          // Primeiro, obtenha o access_token usando o Client ID e Client Secret
+          const authResponse = await axios.post('https://api.imgur.com/oauth2/token', null, {
+            params: {
+              client_id: providerOptions.clientId,
+              client_secret: providerOptions.clientSecret,
+              grant_type: 'client_credentials',
+            },
+          });
+
+          const accessToken = authResponse.data.access_token;
+
+          // Agora, faça o upload da imagem usando o access_token
+          const uploadResponse = await axios.post('https://api.imgur.com/3/image', formData, {
             headers: {
-              Authorization: `Client-ID ${providerOptions.clientId}`,
+              Authorization: `Bearer ${accessToken}`,
               ...formData.getHeaders(),
             },
           });
 
           // Log da resposta da API do Imgur para depuração
-          console.log('Resposta da API do Imgur:', response.data);
+          console.log('Resposta da API do Imgur:', uploadResponse.data);
 
           // Usa a URL direta da imagem retornada pela API
-          file.url = response.data.data.link; // URL direta da imagem
-          file.mime = response.data.data.type;
+          file.url = uploadResponse.data.data.link; // URL direta da imagem
+          file.mime = uploadResponse.data.data.type;
           return file;
         } catch (error) {
           console.error('Erro ao fazer upload para Imgur:', error);
